a094e8bc59df53eebcd5644762f8f80f7f7d6643
Glitch (hello-express) <none>
2020-07-27 15:01:22 +0000

    🚵🐗 Checkpoint ./index.js:19790745/2224 ./package.json:19790745/1389 ./db.js:19790745/267

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const {Telegraf} = require('telegraf');
-const db = require('./db');
+const { Telegraf } = require("telegraf");
+const db = require("./db");
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const welcomeMessage = `Welcome! Ako lang to ang Avengers-Telegram-Bot mo
-Enjoy ka lang at sumaya!`
+Enjoy ka lang at sumaya!`;
 
 const helpMessage = `Kelangang mo ba ng tulong sa Bot?
 Ito ang mga listahan ng command!
 /pasok - para mag log in
 /labas - para mag log out
-/assemble - para i-mention yung lahat ng naka log in`
+/assemble - para i-mention yung lahat ng naka log in`;
 
 var userChat = [];
 var userName = [];
 
-bot.start((ctx) => ctx.reply(welcomeMessage))
+bot.start(ctx => ctx.reply(welcomeMessage));
 
-bot.help((ctx) => ctx.reply(helpMessage))
+bot.help(ctx => ctx.reply(helpMessage));
 
-bot.command("pasok@Avengers_Telegram_Bot", async (ctx) =>{
+bot.command("pasok@avengers_telegram_bot", async ctx => {
   var user = ctx.from.username;
   ctx.reply("Salamat sa pag click sa command @" + user);
-  await db.remove(user, {multi: true});
-  await db.insert(user);
-})
-
-bot.command("labas@Avengers_Telegram_Bot", async (ctx) =>{
-  var user = ctx.from.username;
-  ctx.reply("Salamat sa lahat! Mamimiss ka namin @" + user); 
-  await db.remove(user, {multi: true});
+  await db.remove({ user }, { multi: true });
+  await db.insert({ user });
 });
 
-bot.command("assemble@Avengers_Telegram_Bot", async (ctx) =>{
-})
+bot.command("labas@avengers_telegram_bot", async ctx => {
+  var user = ctx.from.username;
+  ctx.reply("Salamat sa lahat! Mamimiss ka namin @" + user);
+  await db.remove({ user }, { multi: true });
+});
 
-bot.startPolling()
+bot.command("assemble@avengers_telegram_bot", async ctx => {});
+
+bot.startPolling();
